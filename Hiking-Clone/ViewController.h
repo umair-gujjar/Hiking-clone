f0ca7d5e01171b69cbaa47b63c4665dcc8fd3cb8
Chrisbooty <1163209271@qq.com>
2016-05-27 19:42:44 +0800

    Initial Commit

---
+++ Hiking-Clone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hiking-Clone
+//
+//  Created by qianfeng on 16/5/27.
+//  Copyright © 2016年 CJ.W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hiking-Clone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hiking-Clone
+//
+//  Created by qianfeng on 16/5/27.
+//  Copyright © 2016年 CJ.W. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
